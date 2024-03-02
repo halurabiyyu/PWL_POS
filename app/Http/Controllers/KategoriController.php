93356990d02d63467234f418add962a1700b7f58
halurabiyyu <halurabiyyu@gmail.com>
2024-03-02 19:17:44 +0700

    Praktikum 5 - Implementasi Query Builder

---
+++ app/Http/Controllers/KategoriController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+// PRAKTIKUM 5  
+
+class KategoriController extends Controller
+{
+    public function index(){
+        // INSERT DATA 
+        // $data = [
+        //     'kategori_kode' => 'SNK', 
+        //     'kategori_nama' => 'Snack/Makanan Ringan',
+        //     'created_at' => now()
+        // ];
+        // DB::table('m_kategori')->insert($data);
+        // return 'Insert Data Berhasil';
+
+        // UPDATE DATA 
+        // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->update(['kategori_nama' => 'Camilan']);
+        // return "Update data berhasil. Jumlah data yang diupdate" . $row . "baris";
+
+        // DELETE DATA
+        // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->delete();
+        // return 'Delete Data Berhasil. Jumlah Data yang dihapus'. $row . 'baris';
+
+        // SELECT DATA  
+        $data = DB::table('m_kategori')->get();
+        return view('kategori', ['data' => $data]);
+    }
+}
