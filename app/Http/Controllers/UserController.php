3bf463eac10591dda4c9a0390a6f7183201477a7
halurabiyyu <halurabiyyu@gmail.com>
2024-03-05 11:31:30 +0700

    Jobsheet 4 - Praktikum 2.3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -68,7 +68,13 @@ class UserController extends Controller
         // $user = UserModel::findOrFail(1);
         // return view('user', ['data'=> $user]);
 
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        // $user = UserModel::where('username', 'manager9')->firstOrFail();
+        // return view('user', ['data' => $user]);
+
+
+        // JOBSHEET 4 - Praktikum 2.3
+        $user = UserModel::where('level_id', 2)->count();
+        // dd($user);   
         return view('user', ['data' => $user]);
     }
 }
