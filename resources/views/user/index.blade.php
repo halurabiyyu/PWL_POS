0d0d35e14df92c27b529b3fd29f5c8d22044cd43
halurabiyyu <halurabiyyu@gmail.com>
2024-03-28 19:10:06 +0700

    Jobsheet 7 - Praktikum 3

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,14 +1,14 @@
-@extends('layout.app')
+{{-- @extends('layout.app')
 
 {{-- Customize layout section  --}}
 
-@section('subtitle', 'User')
+{{-- @section('subtitle', 'User')
 @section('content_header_title', 'Home')
-@section('content_header_subtitle', 'User')
+@section('content_header_subtitle', 'User') --}}
 
 {{-- Content body: main page content  --}}
 
-@section('content')
+{{-- @section('content')
     <div class="container">
         <div class="card">
             <div class="card-header">Manage User</div>
@@ -22,12 +22,92 @@
             </div>
         </div>
     </div>
+@endsection --}}
+
+
+{{-- @push('scripts')
+    {{ $dataTable->scripts() }}
+@endpush --}}
+
+
+@extends('layout.template')
+
+@section('content')
+    <div class="card card-outline card-primary">
+        <div class="card-header">
+            <h3 class="card-title">{{$page->title}}</h3>
+            <div class="card-tools">
+                <a href="{{url('user/create')}}" class="btn btn-sm btn-primary mt-1">Tambah</a>
+            </div>
+        </div>
+        <div class="card-body">
+            @if (session('success'))
+                <div class="alert alert-success">{{session('success')}}</div>
+            @endif
+            @if (session('error'))
+                <div class="alert alert-danger">{{session('error')}}</div>
+            @endif
+            <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Username</th>
+                        <th>Nama</th>
+                        <th>Level Pengguna</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
 @endsection
 
-
-@push('scripts')
-    {{ $dataTable->scripts() }}
+@push('css')
 @endpush
 
-
-    
+@push('js')
+    <script>
+        $(document).ready(function() { 
+            var dataUser = $('#table_user').DataTable({ 
+            serverSide: true,     // serverSide: true, jika ingin menggunakan server side processing 
+            ajax: { 
+                "url": "{{ url('user/list') }}", 
+                "dataType": "json", 
+                "type": "POST" 
+            }, 
+            columns: [ 
+                { 
+                    data: "DT_RowIndex", // nomor urut dari laravel datatable addIndexColumn()            
+                    className: "text-center", 
+                    orderable: false, 
+                    searchable: false     
+                },
+                { 
+                    data: "username",                
+                    className: "", 
+                    orderable: true,    // orderable: true, jika ingin kolom ini bisa diurutkan 
+                    searchable: true    // searchable: true, jika ingin kolom ini bisa dicari 
+                },
+                { 
+                    data: "nama",                
+                    className: "", 
+                    orderable: true,    // orderable: true, jika ingin kolom ini bisa diurutkan 
+                    searchable: true    // searchable: true, jika ingin kolom ini bisa dicari 
+                },
+                { 
+                    data: "level.level_nama",                
+                    className: "", 
+                    orderable: false,    // orderable: true, jika ingin kolom ini bisa diurutkan 
+                    searchable: false    // searchable: true, jika ingin kolom ini bisa dicari 
+                },
+                { 
+                    data: "aksi",                
+                    className: "", 
+                    orderable: false,    // orderable: true, jika ingin kolom ini bisa diurutkan 
+                    searchable: false    // searchable: true, jika ingin kolom ini bisa dicari 
+                } 
+            ] 
+        }); 
+    }); 
+  </script> 
+@endpush 
--- routes/web.php
+++ routes/web.php
@@ -25,15 +25,15 @@ use App\Http\Controllers\WelcomeController;
 
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index'])->name('/kategori');
-Route::get('/user', [UserController::class, 'index'])->name('/user');
+// Route::get('/user', [UserController::class, 'index'])->name('/user');
 
-// Jobsheet 4 - Praktikum 2.6
+// // Jobsheet 4 - Praktikum 2.6
 
-Route::get('/user/tambah', [UserController::class, 'tambah'])->name('/user/tambah');
-Route::get('user/ubah/{id}', [UserController::class, 'ubah'])->name('/user/ubah');
-Route::get('user/hapus/{id}', [UserController::class, 'hapus'])->name('/user/hapus');
-Route::post('/user', [UserController::class, 'tambah_simpan']);
-Route::put('user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan'])->name('/user/ubah_simpan');
+// Route::get('/user/tambah', [UserController::class, 'tambah'])->name('/user/tambah');
+// Route::get('user/ubah/{id}', [UserController::class, 'ubah'])->name('/user/ubah');
+// Route::get('user/hapus/{id}', [UserController::class, 'hapus'])->name('/user/hapus');
+// Route::post('/user', [UserController::class, 'tambah_simpan']);
+// Route::put('user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan'])->name('/user/ubah_simpan');
 
 // Jobsheet 5 - Praktikum 3  
 
@@ -56,3 +56,15 @@ Route::resource('m_user', POSController::class);
 
 // Jobsheet 7 - Praktikum 2. No 5 
 Route::get('/', [WelcomeController::class, 'index']);
+
+// Jobsheet 7 - Praktikum 3. No 3
+Route::group(['prefix' => 'user'], function(){
+    Route::get('/', [UserController::class, 'index']);
+    Route::post('/list', [UserController::class, 'list']);
+    Route::get('/create', [UserController::class, 'create']);
+    Route::post('/', [UserController::class, 'store']);
+    Route::get('/{id}', [UserController::class, 'show']);
+    Route::get('/{id}/edit', [UserController::class, 'edit']);
+    Route::put('/{id}', [UserController::class, 'update']);
+    Route::delete('/{id}', [UserController::class, 'destroy']);
+});
